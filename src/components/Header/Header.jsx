3b4da17837939ca5ebb2efcb3415ae50158a4837
Sachin Gavali <sachingavali321@gmail.com>
2024-01-01 22:21:28 +0530

    adding components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,67 @@
+import { Container } from "postcss";
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import Logo from "../Logo";
+import LogoutBtn from "../LogoutBtn";
 
 
 const Header = () => {
+
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate();
+    const navItems = [
+        {
+            name: "Home",
+            slug: "/",
+            active: true
+        },
+        {
+            name: "Login",
+            slug: "/login",
+            active: !authStatus
+        },
+        {
+            name: "Sign-up",
+            slug: "/signup",
+            active: !authStatus
+        },
+        {
+            name: "All Posts",
+            slug: "/all-posts",
+            active: authStatus
+        },
+        {
+            name: "Add Posts",
+            slug: "/add-posts",
+            active: authStatus
+        },
+
+    ]
     return (
-        <div>Header</div>
+        <Header>
+            <Container >
+                <nav>
+                    <div>
+                        <slug path="/" >
+                            <Logo width="75px" />
+                        </slug>
+                    </div>
+                    <ul className="w-full border-2 bg-slate-900">
+                        {
+                            navItems.map((item) => item.active ? <li
+                                onClick={() => navigate(item.slug)} key={item.name}> {item.name}  </li> : null)
+                        }
+                        {
+                            authStatus && (
+                                <li>
+                                    <LogoutBtn />
+                                </li>
+                            )
+                        }
+                    </ul>
+                </nav>
+            </Container>
+        </Header>
     )
 }
 
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -12,12 +12,12 @@ const authSlice = createSlice({
 
         login: (state, action) => {
             state.status = true;
-            state.userData = action.payload.userData;
+            state.userData = action.payload;
         },
         logout: (state) => {
             state.status = false;
             state.userData = null;
-        },
+        }
     }
 });
 
