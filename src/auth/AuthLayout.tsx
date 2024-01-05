d95b6757fd2962c2c5f9c7b4e255e0fb6f93e5ed
Sachin Gavali <sachingavali321@gmail.com>
2024-01-05 18:10:57 +0530

    adding auth

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthinticated = false;
+  return (
+    <>
+      {/*  user is authinticated means logined then hi goes on home page other wise authlayout page render  sign in or sign up page  */}
+      {isAuthinticated ? (
+        <Navigate to={"/"} />
+      ) : (
+        <div className=" w-full min-h-screen flex flex-col lg:flex-row">
+          <section className="flex flex-1 justify-center items-center flex-col py-10 mt-5">
+            <Outlet />
+          </section>
+          <img src="/assets/images/side-img.svg" alt="notfound" />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
